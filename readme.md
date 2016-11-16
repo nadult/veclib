0bd0e7b369b7d6781e1acf0b1ad1cef3b68ca691
Krzysztof Jakubowski <nadult@fastmail.fm>
2016-11-16 15:04:59 +0100

    No longer needs to be built (not generating preprocessed header files)
    - Using libfwk insread of baselib for serialization (still optional)
    - Updated readme

---
+++ readme.md
@@ -0,0 +1,7 @@
+# veclib
+
+Veclib is a wrapper library over SIMD compiler intrinsics which make
+SSE code look cleaner and easier to write.
+
+This is a header only library which doesn't require compilation. It's best
+to include it as a submodule.
--- veclib.h
+++ veclib.h
@@ -37,8 +37,6 @@
 
 namespace veclib
 {
-
-#ifndef BASELIB_H
 	namespace basetypes
 	{
 		typedef unsigned char		u8;
@@ -54,34 +52,39 @@ namespace veclib
 		typedef double				f64;
 	}
 	using namespace basetypes;
+
+	#include "vecbase.h"
+	#include "vec2.h"
+	#include "vec3.h"
+	#include "vec4.h"
+
+#ifdef VECLIB_SSE
+	#include "sse/f32.h"
+	#include "sse/vecf32.h"
+	#include "sse/i32.h"
+	#ifndef _mm_shuffle
+		#define _mm_shuffle(mask,v)	_mm_shuffle_<mask>(v)
+	#endif
+#elif defined(VECLIB_ALTIVEC)
+	#include "altivec/f32.h"
+	#include "altivec/i32.h"
+	#include "altivec/vecf32.h"
+#elif defined(__SPU__)
+	#include "spu/f32.h"
+	#include "spu/i32.h"
+	#include "spu/vecf32.h"
 #else
-	namespace basetypes = baselib::basetypes;
-	using namespace baselib::basetypes;
+	#include "scalar/f32.h"
+	#include "scalar/i32.h"
+	#include "scalar/vecf32.h"
 #endif
+
+	#include "matrix.h"
+
 }
 
-
-#if !defined(__IBMCPP__)
-	#ifdef VECLIB_SSE
-		#if VECLIB_SSE >= 0x20
-			#include "vecliball_sse2.h"
-		#elif VECLIB_SSE >= 0x10
-			#include "vecliball_sse.h"
-		#else
-			#error "wrong value of VECLIB_SSE"
-		#endif
-	#elif defined(VECLIB_SPU)
-		#include "vecliball_spu.h"
-	#elif defined(VECLIB_ALTIVEC)
-		#include "vecliball_altivec.h"
-	#else
-		#include "vecliball_scalar.h"
-	#endif
-#else
-	#include "vecliball.h"
-#endif
-
-#ifdef BASELIB_H
+// Include fwk_base.h first if you want serialization support
+#ifdef FWK_BASE_H
 
 SERIALIZE_AS_POD(veclib::Vec2<float>)
 SERIALIZE_AS_POD(veclib::Vec3<float>)
